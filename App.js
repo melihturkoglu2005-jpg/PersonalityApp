6cdd46b5f9433cb8d169e74c931a84be85ba0a9f
melihturkoglu2005-jpg <melihturkoglu2005@gmail.com>
2026-04-05 19:16:29 +0300

    sssss

--- App.js
+++ App.js
@@ -4,32 +4,17 @@ import HomeScreen       from './src/screens/HomeScreen';
 import MBTIScreen       from './src/screens/MBTIScreen';
 import EnneagramScreen  from './src/screens/EnneagramScreen';
 import ResultScreen     from './src/screens/ResultScreen';
-import CydenzScreen     from './src/screens/CydenzScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const linking = {
-    prefixes: ['https://personalityapp.vercel.app', 'http://localhost:19006'],
-    config: {
-      screens: {
-        Home: '',
-        MBTI: 'mbti',
-        Enneagram: 'enneagram',
-        Result: 'result',
-        Cydenz: 'cydenz',
-      },
-    },
-  };
-
   return (
-    <NavigationContainer linking={linking}>
+    <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home"       component={HomeScreen} />
         <Stack.Screen name="MBTI"       component={MBTIScreen} />
         <Stack.Screen name="Enneagram"  component={EnneagramScreen} />
         <Stack.Screen name="Result"     component={ResultScreen} />
-        <Stack.Screen name="Cydenz"     component={CydenzScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
